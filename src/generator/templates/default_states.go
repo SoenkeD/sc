de50947d4f3cfe835b9bc82ea8228e2305b52990
sonked <sonked@hotmail.de>
2024-07-21 23:38:31 +0200

    generated states are now sorted

--- src/generator/templates/default_states.go
+++ src/generator/templates/default_states.go
@@ -2,6 +2,7 @@ package templates
 
 import (
 	"log"
+	"sort"
 	"strings"
 	"text/template"
 
@@ -52,13 +53,32 @@ func GenerateState(stateIdx, oneStateTpl string, state stage2.ParsedState) (stri
 	return code, nil
 }
 
+func sortStateMap(states map[string]stage2.ParsedState) ([]string, []stage2.ParsedState) {
+
+	keys := make([]string, 0, len(states))
+	for k := range states {
+		keys = append(keys, k)
+	}
+
+	sort.Strings(keys)
+
+	sortedList := make([]stage2.ParsedState, 0, len(states))
+	for _, key := range keys {
+		sortedList = append(sortedList, states[key])
+	}
+
+	return keys, sortedList
+}
+
 func GenerateStates(uml stage2.ParseUmlStage2, tpl GenerateTemplatesInput) ([]string, error) {
 
 	stateCodes := []string{}
 
-	for stateIdx, state := range uml.States {
+	keys, sortedList := sortStateMap(uml.States)
 
-		stateCode, err := GenerateState(stateIdx, tpl.TemplatedBaseFiles["onestate"], state)
+	for stateIdx, state := range sortedList {
+
+		stateCode, err := GenerateState(keys[stateIdx], tpl.TemplatedBaseFiles["onestate"], state)
 		if err != nil {
 			return nil, err
 		}
